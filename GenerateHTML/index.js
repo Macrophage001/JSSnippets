d482eae1baf0ee1d701991e708f86309bb464f06
Macrophage001 <102617650+ronindc@users.noreply.github.com>
2022-05-07 11:04:43 -0400

    - added another method of using function.

--- GenerateHTML/index.js
+++ GenerateHTML/index.js
@@ -36,7 +36,9 @@ button.onclick = () => console.log("Ya clicked me didn't ya!");
 
 body.append(button);
 
-// Nesting 'generateHTML' calls.
+/*
+    Nesting 'generateHTML' calls.
+*/
 let form = generateHTML(`
     <form action='javascript:void(0);'>
         ${generateHTML('<input id="name-input" type="text" placeholder="Name">').outerHTML}
@@ -51,3 +53,46 @@ let submitBtn = form.querySelector('#submit-btn');
 submitBtn.onclick = () => console.log(`${name.value}, ${addr.value}`);
 
 body.append(form);
+
+
+/*
+    Applying conditional checks to display different elements, taken from my 'Endless Runner' project.
+*/
+const endCardTemplate = (state, points) => {
+    let endCardDiv = generateHTML(`
+        <div class="end-card end-card-animation">
+            <h2>
+                ${
+                    state === 'WIN'
+                    ? 'You Win!'
+                    : 'You Lose!'
+                }
+            </h2>
+            <div class="end-score"><h2>Score: ${points}</h2></div>
+            <div class="controls">
+                ${
+                    state === 'WIN'
+                    ? generateHTML('<button id="next-level-btn"><h2>Next Level</h2></button>').outerHTML
+                    : generateHTML('<button id="restart-btn"><h2>Restart</h2></button>').outerHTML
+                }
+            </div>
+        </div>
+    `);
+
+    let nextLevelBtn = endCardDiv.querySelector('#next-level-btn');
+    if (nextLevelBtn !== null) {
+        nextLevelBtn.addEventListener('click', () => console.log('Starting next level...'));
+        console.log(nextLevelBtn);
+    }
+
+    let restartBtn = endCardDiv.querySelector('#restart-btn');
+    if (restartBtn !== null) {
+        restartBtn.addEventListener('click', () => console.log('Restarting level...'));
+        console.log(restartBtn);
+    }
+
+    return endCardDiv;
+}
+
+body.append(endCardTemplate('WIN', 100));
+body.append(endCardTemplate('LOSE', 100));
